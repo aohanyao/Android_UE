27e4f13020c480e9ecc4e2c9e6085da298edeae3
江俊超 <928692385@qq.com>
2018-12-09 13:10:46 +0800

    # update:增加Fling

--- Coder/L011/app/src/main/java/com/jc/adv/l11/ui/ScalableView.java
+++ Coder/L011/app/src/main/java/com/jc/adv/l11/ui/ScalableView.java
@@ -7,10 +7,13 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.support.v4.view.GestureDetectorCompat;
+import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.OverScroller;
 
 import com.jc.adv.l11.R;
 import com.jc.adv.l11.util.Utils;
@@ -21,7 +24,8 @@ import com.jc.adv.l11.util.Utils;
  * Description:
  * ChangeLog:
  */
-public class ScalableView extends View implements GestureDetector.OnDoubleTapListener, GestureDetector.OnGestureListener {
+public class ScalableView extends View implements GestureDetector.OnDoubleTapListener, GestureDetector.OnGestureListener, Runnable {
+    private final String TAG = getClass().getSimpleName();
 
     private Bitmap mBitmap;
     private Paint mPaint;
@@ -33,12 +37,16 @@ public class ScalableView extends View implements GestureDetector.OnDoubleTapLis
     // 最大缩放
     private float mMaxScale;
 
-    private float mOffsetX;
-    private float mOffsetY;
-    private float mOriginalOffsetX;
+    private float mOriginOffsetX = 0;
+    private float mOffsetX = 0;
+    private float mOriginOffsetY = 0;
+    private float mOffsetY = 0;
+
     private float mBitmatOriginalOffsetX;
     private float mBitmatOriginalOffsetY;
-    private float mOriginalOffsetY;
+
+    private float mDownX;
+    private float mDownY;
 
     private boolean isBig = false;
 
@@ -48,6 +56,8 @@ public class ScalableView extends View implements GestureDetector.OnDoubleTapLis
     private ObjectAnimator scaleAmin;
 
     private float tempScale = 1.5f;
+
+    private OverScroller scroller;
 
 
     public ScalableView(Context context) {
@@ -70,9 +80,9 @@ public class ScalableView extends View implements GestureDetector.OnDoubleTapLis
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         // 定义两个中间值
-        mOriginalOffsetX = (getWidth() - mImageSize) / 2;
+//        mOriginalOffsetX = (getWidth() - mImageSize) / 2;
         mBitmatOriginalOffsetX = (getWidth() - mImageSize) / 2;
-        mOriginalOffsetY = (getHeight() - mImageSize) / 2;
+//        mOriginalOffsetY = (getHeight() - mImageSize) / 2;
         mBitmatOriginalOffsetY = (getHeight() - mImageSize) / 2;
 
         // 两种缩放方式：
@@ -103,13 +113,14 @@ public class ScalableView extends View implements GestureDetector.OnDoubleTapLis
         //
         gestureDetectorCompat = new GestureDetectorCompat(getContext(), this);
         gestureDetectorCompat.setOnDoubleTapListener(this);
+        scroller = new OverScroller(getContext());
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         // 进行触摸偏移
-        canvas.translate(mOffsetX * scalePre, mOffsetY * scalePre);
+        canvas.translate(mOriginOffsetX * scalePre, mOriginOffsetY * scalePre);
 
 
         float scale = mMixScale + ((mMaxScale - mMixScale) * scalePre);
@@ -132,14 +143,17 @@ public class ScalableView extends View implements GestureDetector.OnDoubleTapLis
 
     @Override
     public boolean onDoubleTap(MotionEvent e) {
+        // 进行判断，进行双击中的点 进行中心进行缩放
         isBig = !isBig;
+        Log.e(TAG, "onDoubleTapEvent: " + e.getX());
+
+
         if (isBig) {
             // 从小到大
+//            Log.e(TAG, "onDoubleTap: " + mOffsetX);
             getScaleAmin().start();
         } else {
             // 从大到小
-//            mOffsetY=0;
-//            mOffsetX=0;
             // 进行回滚，然后进行X轴的变化
             getScaleAmin().reverse();
         }
@@ -148,6 +162,22 @@ public class ScalableView extends View implements GestureDetector.OnDoubleTapLis
 
     @Override
     public boolean onDoubleTapEvent(MotionEvent e) {
+
+//        if (!isBig) {
+//            switch (e.getActionMasked()) {
+//                case MotionEvent.ACTION_DOWN:
+//                    mDownX = e.getX();
+//                    mDownY = e.getY();
+//                    break;
+//                case MotionEvent.ACTION_UP:
+////                    int offsetX =
+//                    mOffsetX = e.getX();
+//                    mOriginOffsetY = e.getY();
+//                    break;
+//            }
+//        }
+
+
         return false;
     }
 
@@ -174,12 +204,11 @@ public class ScalableView extends View implements GestureDetector.OnDoubleTapLis
         if (isBig) {// 在放大的状态下才进行拖动
             //进行了滚动
             // 以(0,0)为原点
-            mOffsetX -= distanceX;
-            mOffsetY -= distanceY;
+            mOriginOffsetX -= distanceX;
+            mOriginOffsetY -= distanceY;
             // 边界判断
             // 最右不能小于图片的宽度减去View的宽度除以二，最左不能大于图片的宽度减去View的宽度除以二
             // 最上和最下是一个道理
-            // TODO 2018年12月04日22:33:56 明天进行复刻
             handlerPort();
 
 
@@ -194,20 +223,20 @@ public class ScalableView extends View implements GestureDetector.OnDoubleTapLis
      */
     private void handlerPort() {
         float offsetLimitX = (mBitmap.getWidth() * mMaxScale - getWidth()) / 2;
-        if (mOffsetX <= -offsetLimitX) {
-            mOffsetX = Math.max(mOffsetX, -offsetLimitX);
+        if (mOriginOffsetX <= -offsetLimitX) {
+            mOriginOffsetX = Math.max(mOriginOffsetX, -offsetLimitX);
         }
-        if (mOffsetX >= offsetLimitX) {
-            mOffsetX = Math.min(mOffsetX, offsetLimitX);
+        if (mOriginOffsetX >= offsetLimitX) {
+            mOriginOffsetX = Math.min(mOriginOffsetX, offsetLimitX);
         }
 
         // 上下边界的判定
         float offsetLimitY = (mBitmap.getHeight() * mMaxScale - getHeight()) / 2;
-        if (mOffsetY <= -offsetLimitY) {
-            mOffsetY = Math.max(mOffsetY, -offsetLimitY);
+        if (mOriginOffsetY <= -offsetLimitY) {
+            mOriginOffsetY = Math.max(mOriginOffsetY, -offsetLimitY);
         }
-        if (mOffsetY >= offsetLimitY) {
-            mOffsetY = Math.min(mOffsetY, offsetLimitY);
+        if (mOriginOffsetY >= offsetLimitY) {
+            mOriginOffsetY = Math.min(mOriginOffsetY, offsetLimitY);
         }
     }
 
@@ -218,6 +247,23 @@ public class ScalableView extends View implements GestureDetector.OnDoubleTapLis
 
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+
+        if (isBig) {
+            Log.e(TAG, "onFling: ");
+            float offsetLimitX = (mBitmap.getWidth() * mMaxScale - getWidth()) / 2;
+            float offsetLimitY = (mBitmap.getHeight() * mMaxScale - getHeight()) / 2;
+
+            scroller.fling((int) mOffsetX,
+                    (int) mOffsetY,
+                    (int) velocityX,
+                    (int) velocityY,
+                    (int) -offsetLimitX,
+                    (int) offsetLimitX,
+                    (int) -offsetLimitY,
+                    (int) offsetLimitY);
+            ViewCompat.postInvalidateOnAnimation(this);
+        }
+
         return false;
     }
 
@@ -236,5 +282,15 @@ public class ScalableView extends View implements GestureDetector.OnDoubleTapLis
         this.scalePre = scalePre;
         handlerPort();
         postInvalidate();
+    }
+
+    @Override
+    public void run() {
+        if (scroller.computeScrollOffset()) {
+            mOffsetX = scroller.getCurrX();
+            mOffsetY = scroller.getCurrY();
+            invalidate();
+            ViewCompat.postInvalidateOnAnimation(this);
+        }
     }
 }
