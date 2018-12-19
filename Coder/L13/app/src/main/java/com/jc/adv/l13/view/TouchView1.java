1f071ee1b321efd28bf7467576a980314ae6df42
江俊超 <aohanyao@gmail.com>
2018-12-19 17:45:46 +0800

    1. 完成多点触摸的第一个例子 2. 完成边界判定

--- Coder/L13/app/src/main/java/com/jc/adv/l13/view/TouchView1.java
+++ Coder/L13/app/src/main/java/com/jc/adv/l13/view/TouchView1.java
@@ -21,11 +21,15 @@ public class TouchView1 extends View {
     private Bitmap mBitmap;
     private Paint mPaint;
     float mBitmapWidth = Utils.dp2px(200);
-
+    private final String TAG = getClass().getSimpleName();
     private float mTranX = 0;
     private float mTranY = 0;
     private float downX;
     private float downY;
+
+    private int trankId;
+    private int mHeight;
+    private int mWidth;
 
     public TouchView1(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -44,29 +48,75 @@ public class TouchView1 extends View {
         super.onSizeChanged(w, h, oldw, oldh);
         mTranX = getWidth() / 2 - mBitmap.getWidth() / 2;
         mTranY = getHeight() / 2 - mBitmap.getHeight() / 2;
+        mWidth = getWidth();
+        mHeight = getHeight();
     }
 
+    // 一个手指，进行拖动
+    // 两个手指，第二个手指进行拖动，第二个手指抬起，第一个手指拖动
+    // 三个手指，第三个手机进行拖动，第三个手指抬起，第二个手指拖动，第二个手指抬起，第一个手指拖动
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getActionMasked()) {
             case MotionEvent.ACTION_DOWN:
+                trankId = event.getPointerId(event.getActionIndex());
                 downX = event.getX();
                 downY = event.getY();
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                trankId = event.getPointerId(event.getActionIndex());
+                int downIndex = event.getActionIndex();
+                downX = event.getX(downIndex);
+                downY = event.getY(downIndex);
                 break;
             case MotionEvent.ACTION_MOVE:
-                mTranX = event.getX() - downX + mTranX;
-                mTranY = event.getY() - downY + mTranY;
-                invalidate();
-                downX = event.getX();
-                downY = event.getY();
+                int moveIndex = event.findPointerIndex(trankId);
+                if (moveIndex >= 0) {
+                    mTranX = event.getX(moveIndex) - downX + mTranX;
+                    mTranY = event.getY(moveIndex) - downY + mTranY;
+                    handlerPort();
+                    invalidate();
+                    downX = event.getX(moveIndex);
+                    downY = event.getY(moveIndex);
+
+                }
                 break;
             case MotionEvent.ACTION_POINTER_UP:
+                // 抬起的是当前的手指
+                if (event.getPointerId(event.getActionIndex()) == trankId && event.getPointerCount() > 1) {
+                    int newIndex;
+
+                    if (event.getActionIndex() == event.getPointerCount() - 1 && event.getPointerCount() > 2) {
+                        newIndex = event.getActionIndex() - 2;
+                    } else {
+                        newIndex = event.getActionIndex() - 1;
+                    }
+
+                    if (newIndex >= 0) {
+                        trankId = event.getPointerId(newIndex);
+                        downX = event.getX(newIndex);
+                        downY = event.getY(newIndex);
+                    }
+                }
+
                 break;
 
         }
 
 
         return true;
+    }
+
+    /**
+     * 处理边界情况
+     */
+    void handlerPort() {
+        mTranX = Math.max(0, mTranX);
+        mTranX = Math.min(mWidth - mBitmap.getWidth(), mTranX);
+
+
+        mTranY = Math.max(0, mTranY);
+        mTranY = Math.min(mHeight - mBitmap.getHeight(), mTranY);
     }
 
     @Override
